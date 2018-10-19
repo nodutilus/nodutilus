510e083d49bbb848c65789cb2e8aac5d05a1996f
IgorNovozhilov <maildrakon@gmail.com>
2018-10-19 23:35:37 +0300

    class CLArguments

--- source/@ndk/env/ndk.env.js
+++ source/@ndk/env/ndk.env.js
@@ -1,6 +1,72 @@
 /** @module @ndk/env */
 'use strict';
 
-const { dirname, join } = require('path');
-const { existsSync } = require('fs');
-const { homedir } = require('os');
+const clArgDashRE = /^--?/;
+const clArgNameRE = /^(\w+)=(.*)$/;
+
+
+class CLArguments {
+
+  static parse(string = '') {
+    const args = typeof string === 'string' ? string.split(' ') : string.slice();
+    const namedArgs = {};
+    const ordinalArgs = [];
+    while (args.length > 0) {
+      const arg = args.shift();
+      if (clArgDashRE.test(arg)) {
+        const argName = arg.replace(clArgDashRE, '');
+        if (clArgNameRE.test(argName)) {
+          const [, exName, exValue] = clArgNameRE.exec(argName);
+          namedArgs[exName] = exValue;
+        } else {
+          const nextArg = args[0];
+          if (clArgDashRE.test(nextArg)) {
+            namedArgs[argName] = true;
+          } else {
+            args.shift();
+            namedArgs[argName] = nextArg;
+          }
+        }
+      } else {
+        if (arg) {
+          ordinalArgs.push(arg);
+        }
+      }
+    }
+    return { namedArgs, ordinalArgs };
+  }
+
+  static stringify({ namedArgs = {}, ordinalArgs = [] }) {
+    let args = [...ordinalArgs];
+    for (const [name, value] of Object.entries(namedArgs)) {
+      if (value !== false) {
+        args.push('--' + name);
+        if (value !== true) {
+          args.push(value);
+        }
+      }
+    }
+    return args.join(' ');
+  }
+
+  constructor(string = '') {
+    this.parse(string);
+  }
+
+  parse(string) {
+    const { namedArgs, ordinalArgs } = CLArguments.parse(string);
+    this.namedArgs = namedArgs;
+    this.ordinalArgs = ordinalArgs;
+  }
+
+  stringify() {
+    return CLArguments.stringify({
+      namedArgs: this.namedArgs,
+      ordinalArgs: this.ordinalArgs
+    });
+  }
+
+}
+
+
+exports.CLArguments = CLArguments;
--- source/tests/@ndk/env/index.js
+++ source/tests/@ndk/env/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const { equal } = require('assert').strict;
+const { equal, deepEqual } = require('assert').strict;
 
 const { Test } = require('@ndk/test');
-const env = require('@ndk/env');
+const { CLArguments } = require('@ndk/env');
 
 
 class NDKEnv extends Test {
@@ -12,11 +12,27 @@ class NDKEnv extends Test {
     return '@ndk/env';
   }
 
-  ['test: Empty']() {
-    equal(1, 1);
+  ['test: CLArguments.parse']() {
+    deepEqual(CLArguments.parse(), { namedArgs: {}, ordinalArgs: [] });
+    deepEqual(CLArguments.parse('a b c').ordinalArgs, ['a', 'b', 'c']);
+    deepEqual(CLArguments.parse(['-a', '--b', 'c', '-d=e']).namedArgs, { a: true, b: 'c', d: 'e' });
   }
 
+  ['test: CLArguments.stringify']() {
+    equal(CLArguments.stringify({ namedArgs: { a: 'b', 'c': true } }), '--a b --c');
+    equal(CLArguments.stringify({ ordinalArgs: ['a', 'b', 'c'] }), 'a b c');
+    equal(CLArguments.stringify({
+      namedArgs: { a: 'b', 'c': true },
+      ordinalArgs: ['a', 'b', 'c']
+    }), 'a b c --a b --c');
+    equal(CLArguments.stringify({ namedArgs: { a: false } }), '');
+  }
 
+  ['test: CLArguments.constructor']() {
+    deepEqual(new CLArguments().namedArgs, {});
+    deepEqual(new CLArguments().ordinalArgs, []);
+    equal(new CLArguments('a b c').stringify(), 'a b c');
+  }
 }
 
 

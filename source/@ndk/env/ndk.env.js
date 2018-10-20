b57df3384a4a081541ca1547a1922f1f52bf2fa4
IgorNovozhilov <maildrakon@gmail.com>
2018-10-21 01:26:42 +0300

    все в классе

--- source/@ndk/env/ndk.env.js
+++ source/@ndk/env/ndk.env.js
@@ -1,39 +1,48 @@
 /** @module @ndk/env */
 'use strict';
 
-const clArgDashRE = /^--?/;
-const clArgNameRE = /^(\w+)=(.*)$/;
-
 
 class CLArguments {
 
-  static parse(string = '') {
+  static get clArgDashRE() {
+    return /^--?/;
+  }
+
+  static get clArgNameRE() {
+    return /^(\w+)=(.*)$/;
+  }
+
+  static parse(string = '', {
+    clArgDashRE = CLArguments.clArgDashRE,
+    clArgNameRE = CLArguments.clArgNameRE
+  } = {}) {
     const args = typeof string === 'string' ? string.split(' ') : string;
-    const namedArgs = {};
-    const ordinalArgs = [];
+    const clFlags = {};
+    const clOptions = {};
+    const clArguments = [];
     for (let index = 0; index < args.length; index++) {
       const arg = args[index];
       if (clArgDashRE.test(arg)) {
         const argName = arg.replace(clArgDashRE, '');
         if (clArgNameRE.test(argName)) {
           const [, exName, exValue] = clArgNameRE.exec(argName);
-          namedArgs[exName] = exValue;
+          clOptions[exName] = exValue;
         } else {
           const nextArg = args[index + 1];
           if (clArgDashRE.test(nextArg)) {
-            namedArgs[argName] = true;
+            clFlags[argName] = true;
           } else {
             index++;
-            namedArgs[argName] = nextArg;
+            clOptions[argName] = nextArg;
           }
         }
       } else {
         if (arg) {
-          ordinalArgs.push(arg);
+          clArguments.push(arg);
         }
       }
     }
-    return { namedArgs, ordinalArgs };
+    return { clFlags, clOptions, clArguments };
   }
 
   static stringify({ namedArgs = {}, ordinalArgs = [] }) {

87acfadeadd3922e8a490ad230070f1a23a70eda
ia.novozhilov <ia.novozhilov@tensor.ru>
2018-10-23 17:32:34 +0300

    Новые имена параметров и тесты для них

--- source/@ndk/env/ndk.env.js
+++ source/@ndk/env/ndk.env.js
@@ -4,56 +4,118 @@
 
 class CLArguments {
 
-  static get clArgDashRE() {
+  /**
+   * @name CLArguments.prefixPattern
+   * @type {RegExp}
+   * @default /^--?/
+   */
+  static get prefixPattern() {
     return /^--?/;
   }
 
-  static get clArgNameRE() {
-    return /^(\w+)=(.*)$/;
+  /**
+   * @name CLArguments.setterPattern
+   * @type {RegExp}
+   * @default /=/
+   */
+  static get setterPattern() {
+    return /=/;
   }
 
-  static parse(string = '', {
-    clArgDashRE = CLArguments.clArgDashRE,
-    clArgNameRE = CLArguments.clArgNameRE
+  /**
+   * @typedef CLArguments~parseOptions
+   * @prop {RegExp} [prefixPattern=CLArguments.prefixPattern]
+   * @prop {RegExp} [setterPattern=CLArguments.setterPattern]
+   */
+  /**
+   * @typedef CLArguments~solvedArgument
+   * @prop {string} name
+   * @prop {string} value
+   * @prop {boolean} offset
+   */
+  /**
+   * @method CLArguments.resolveArgument
+   * @param {string} testName
+   * @param {string} [testValue]
+   * @param {CLArguments~parseOptions} options
+   * @returns {CLArguments~solvedArgument}
+   */
+  static resolveArgument(testName, testValue, {
+    prefixPattern = this.prefixPattern,
+    setterPattern = this.setterPattern
   } = {}) {
-    const args = typeof string === 'string' ? string.split(' ') : string;
-    const clFlags = {};
-    const clOptions = {};
-    const clArguments = [];
-    for (let index = 0; index < args.length; index++) {
-      const arg = args[index];
-      if (clArgDashRE.test(arg)) {
-        const argName = arg.replace(clArgDashRE, '');
-        if (clArgNameRE.test(argName)) {
-          const [, exName, exValue] = clArgNameRE.exec(argName);
-          clOptions[exName] = exValue;
-        } else {
-          const nextArg = args[index + 1];
-          if (clArgDashRE.test(nextArg)) {
-            clFlags[argName] = true;
-          } else {
-            index++;
-            clOptions[argName] = nextArg;
-          }
-        }
+    const result = {};
+    if (prefixPattern.test(testName)) {
+      const name = testName.replace(prefixPattern, '');
+      if (setterPattern.test(name)) {
+        const setter = name.replace(setterPattern, ' ').split(' ');
+        result.name = setter[0];
+        result.value = setter[1];
+      } else if (typeof testValue === 'undefined' || prefixPattern.test(testValue)) {
+        result.name = name;
       } else {
-        if (arg) {
-          clArguments.push(arg);
-        }
+        result.name = name;
+        result.value = testValue;
+        result.offset = true;
+      }
+    } else {
+      result.value = testName;
+    }
+    return result;
+  }
+
+  /**
+   * @typedef CLArguments~parsedArguments
+   * @prop {Object<boolean>} flags
+   * @prop {Object<string>} options
+   * @prop {Array<string>} args
+   */
+  /**
+   * @method CLArguments.parse
+   * @param {string|Array<string>} [input=[]]
+   * @param {CLArguments~parseOptions} options
+   * @returns {CLArguments~parsedArguments}
+   */
+  static parse(input = [], options) {
+    const inputArgs = typeof input === 'string' ? input.split(' ').filter(Boolean) : input;
+    const parsed = { flags: {}, options: {}, args: [] };
+    for (let index = 0; index < inputArgs.length; index++) {
+      const { name, value, offset } = this.resolveArgument(
+        inputArgs[index], inputArgs[index + 1], options
+      );
+      if (name && value) {
+        parsed.options[name] = value;
+      } else if (name) {
+        parsed.flags[name] = true;
+      } else {
+        parsed.args.push(value);
+      }
+      if (offset) {
+        index++;
       }
     }
-    return { clFlags, clOptions, clArguments };
+    return parsed;
   }
 
-  static stringify({ namedArgs = {}, ordinalArgs = [] }) {
-    let args = [...ordinalArgs];
-    for (const [name, value] of Object.entries(namedArgs)) {
-      if (value !== false) {
-        args.push('--' + name);
-        if (value !== true) {
-          args.push(value);
-        }
+  /**
+   * @method CLArguments.stringify
+   * @param {CLArguments~parsedArguments} parsedArguments
+   * @returns {string}
+   */
+  static stringify(parsedArguments) {
+    const args = [];
+    if ('flags' in parsedArguments) {
+      for (const [name] of Object.entries(parsedArguments.flags)) {
+        args.push('-' + name);
       }
+    }
+    if ('options' in parsedArguments) {
+      for (const [name, value] of Object.entries(parsedArguments.options)) {
+        args.push(`--${name}=${value}`);
+      }
+    }
+    if ('args' in parsedArguments) {
+      args.push(...parsedArguments.args);
     }
     return args.join(' ');
   }
--- source/tests/@ndk/env/index.js
+++ source/tests/@ndk/env/index.js
@@ -12,23 +12,87 @@ class NDKEnv extends Test {
     return '@ndk/env';
   }
 
+  ['test: CLArguments.resolveArgument']() {
+    // Флаги
+    deepEqual(CLArguments.resolveArgument('--a'), { name: 'a' });
+    deepEqual(CLArguments.resolveArgument('-a'), { name: 'a' });
+    deepEqual(CLArguments.resolveArgument('--a', '-b'), { name: 'a' });
+    deepEqual(CLArguments.resolveArgument('--a', '-b=c'), { name: 'a' });
+    // Опции
+    deepEqual(CLArguments.resolveArgument('--a', 'b'), { name: 'a', value: 'b', offset: true });
+    deepEqual(CLArguments.resolveArgument('-a', 'b'), { name: 'a', value: 'b', offset: true });
+    // Присвоение значения опции
+    deepEqual(CLArguments.resolveArgument('-a=b'), { name: 'a', value: 'b' });
+    deepEqual(CLArguments.resolveArgument('--a=b'), { name: 'a', value: 'b' });
+    deepEqual(CLArguments.resolveArgument('--a=b', 'c'), { name: 'a', value: 'b' });
+    deepEqual(CLArguments.resolveArgument('--a=b', '-c'), { name: 'a', value: 'b' });
+    deepEqual(CLArguments.resolveArgument('--a=b', '-c=e'), { name: 'a', value: 'b' });
+    // Аргументы
+    deepEqual(CLArguments.resolveArgument('a', 'b'), { value: 'a' });
+  }
+
   ['test: CLArguments.parse']() {
-    deepEqual(CLArguments.parse(), { namedArgs: {}, ordinalArgs: [] });
-    deepEqual(CLArguments.parse('a b c').ordinalArgs, ['a', 'b', 'c']);
-    deepEqual(CLArguments.parse(['-a', '--b', 'c', '-d=e']).namedArgs, { a: true, b: 'c', d: 'e' });
+    // Пустые значения
+    deepEqual(CLArguments.parse(), { flags: {}, options: {}, args: [] });
+    deepEqual(CLArguments.parse(' ', {}), { flags: {}, options: {}, args: [] });
+    // Флаги
+    deepEqual(CLArguments.parse('-a --b', {}).flags, { a: true, b: true });
+    // Опции
+    deepEqual(CLArguments.parse('-a b --c=d', {}).options, { a: 'b', c: 'd' });
+    deepEqual(CLArguments.parse('--a==b', {}).options, { a: '=b' });
+    deepEqual(CLArguments.parse('--a=b=c', {}).options, { a: 'b=c' });
+    // Аргументы
+    deepEqual(CLArguments.parse('a b c', {}).args, ['a', 'b', 'c']);
+    // Флаги + опции
+    deepEqual(CLArguments.parse('-a --b -c=d --e f', {}), {
+      flags: { a: true, b: true },
+      options: { c: 'd', e: 'f' },
+      args: []
+    });
+    deepEqual(CLArguments.parse('-a=--b -c --d', {}), {
+      flags: { c: true, d: true },
+      options: { a: '--b' },
+      args: []
+    });
+    deepEqual(CLArguments.parse('-a b --c=d -e', {}), {
+      flags: { e: true },
+      options: { a: 'b', c: 'd' },
+      args: []
+    });
+    // Флаги + аргументы
+    deepEqual(CLArguments.parse('a --b', {}), {
+      flags: { b: true },
+      options: {},
+      args: ['a']
+    });
+    // Флаги + опции + аргументы
+    deepEqual(CLArguments.parse('x -a b y -z --c=d', {}), {
+      flags: { z: true },
+      options: { a: 'b', c: 'd' },
+      args: ['x', 'y']
+    });
   }
 
   ['test: CLArguments.stringify']() {
-    equal(CLArguments.stringify({ namedArgs: { a: 'b', 'c': true } }), '--a b --c');
-    equal(CLArguments.stringify({ ordinalArgs: ['a', 'b', 'c'] }), 'a b c');
+    // Пустой
+    equal(CLArguments.stringify({}), '');
+    // Флаги
+    equal(CLArguments.stringify({ flags: { 'a': true } }), '-a');
+    equal(CLArguments.stringify({ flags: { 'a': 'b' } }), '-a');
+    // Опции
+    equal(CLArguments.stringify({ options: { 'a': 'b' } }), '--a=b');
+    equal(CLArguments.stringify({ options: { 'a': true } }), '--a=true');
+    // Аргументы
+    equal(CLArguments.stringify({ args: ['a', 'b', 'c'] }), 'a b c');
+    // Флаги + опции + аргументы
     equal(CLArguments.stringify({
-      namedArgs: { a: 'b', 'c': true },
-      ordinalArgs: ['a', 'b', 'c']
-    }), 'a b c --a b --c');
-    equal(CLArguments.stringify({ namedArgs: { a: false } }), '');
+      flags: { a: true },
+      options: { b: 'c', d: '=e' },
+      args: ['f']
+    }), '-a --b=c --d==e f');
   }
 
-  ['test: CLArguments.constructor']() {
+  ['_test: CLArguments.constructor']() {
     deepEqual(new CLArguments().namedArgs, {});
     deepEqual(new CLArguments().ordinalArgs, []);
     equal(new CLArguments('a b c').stringify(), 'a b c');

681eee00c6e093423de1a370c88eac903ef9a471
IgorNovozhilov <maildrakon@gmail.com>
2019-01-26 20:08:54 +0300

    npm i

--- bin/ci/install.js
+++ bin/ci/install.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const { execSync } = require('child_process');
+
+
+execSync('npm i github:nd-toolkit/ndk-project github:nd-toolkit/source-builder', {
+  input: 'inherit',
+  stdio: 'inherit',
+  encoding: 'utf-8'
+});
+
 console.log('Готово.');

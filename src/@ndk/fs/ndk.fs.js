61b80b4d6df7ecb5d696f0f752bc1d0f178d8cc7
IgorNovozhilov <maildrakon@gmail.com>
2019-09-13 00:33:25 +0300

    init copy

--- src/@ndk/fs/ndk.fs.js
+++ src/@ndk/fs/ndk.fs.js
@@ -1,8 +1,11 @@
 /** @module @ndk/fs */
 'use strict'
 
-const { join } = require('path')
-const { readdir } = require('fs').promises
+const { dirname, join } = require('path')
+const {
+  promises: { copyFile, mkdir, readdir, stat },
+  constants: { COPYFILE_EXCL }
+} = require('fs')
 
 const WALK_FILE_FIRST = 0b1
 
@@ -28,9 +31,9 @@ async function walk(path, flags, walker) {
 
 /**
  * @typedef WalkOptions
- * @property {string} options.root
- * @property {number} options.flags
- * @property {Walker} options.walker
+ * @property {string} root
+ * @property {number} [flags]
+ * @property {Walker} walker
  */
 /**
  * @param {string} path
@@ -61,5 +64,47 @@ async function __walk(path, options) {
 }
 
 
+/**
+ * @param {string} src
+ * @param {string} dest
+ * @param {number} flags
+ */
+async function copy(src, dest, flags) {
+  const srcStat = await stat(src)
+
+  if (srcStat.isDirectory()) {
+    await __copy(src, dest, flags)
+  } else {
+    if (!(flags & COPYFILE_EXCL)) {
+      await mkdir(dirname(dest), { recursive: true })
+    }
+    await copyFile(src, dest, flags)
+  }
+}
+
+
+/**
+ * @param {string} src
+ * @param {string} dest
+ * @param {number} flags
+ */
+async function __copy(src, dest, flags) {
+  const mkdirOptions = { recursive: !(flags & COPYFILE_EXCL) }
+
+  await mkdir(dest, mkdirOptions)
+  await __walk('.', {
+    root: src,
+    walker: async (path, dirent) => {
+      if (dirent.isDirectory()) {
+        await mkdir(join(dest, path), mkdirOptions)
+      } else {
+        await copyFile(join(src, path), join(dest, path), flags)
+      }
+    }
+  })
+}
+
+
+exports.copy = copy
 exports.walk = walk
 exports.WALK_FILE_FIRST = WALK_FILE_FIRST
--- test/@ndk/fs/ndk.fs.js
+++ test/@ndk/fs/ndk.fs.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const { Test, assert } = require('@ndk/test')
-const { walk, WALK_FILE_FIRST } = require('@ndk/fs')
+const { copy, walk, WALK_FILE_FIRST } = require('@ndk/fs')
 const { normalize } = require('path')
+const { mkdir, rmdir } = require('fs').promises
 
 
 exports['@ndk/fs'] = class FsTest extends Test {
@@ -19,9 +20,9 @@ exports['@ndk/fs'] = class FsTest extends Test {
       normalize('p2/p2f1.txt')
     ]
 
-    await walk('test/example/fs/walk', (path => {
+    await walk('test/example/fs/walk', path => {
       files.push(path)
-    }))
+    })
 
     assert.deepEqual(files, expected)
   }
@@ -60,13 +61,52 @@ exports['@ndk/fs'] = class FsTest extends Test {
       'p2', false
     ]
 
-    await walk('test/example/fs/walk', WALK_FILE_FIRST, (async (path, dirent) => {
+    await walk('test/example/fs/walk', WALK_FILE_FIRST, async (path, dirent) => {
       await new Promise(resolve => setTimeout(resolve, 1))
       files.push(path)
       files.push(dirent.isFile())
-    }))
+    })
 
     assert.deepEqual(files, expected)
+  }
+
+  /** купируем в несуществующую папку */
+  async ['copy - базовый']() {
+    const filesA = []
+    const filesB = []
+
+    await copy('test/example/fs/walk', 'test/example/fs/copy')
+    await walk('test/example/fs/walk', path => filesA.push(path))
+    await walk('test/example/fs/copy', path => filesB.push(path))
+    await rmdir('test/example/fs/copy', { recursive: true })
+
+    assert.deepEqual(filesB, filesA)
+  }
+
+  /** купируем в существующую папку с заменой */
+  /** купируем в существующую папку с ошибкой */
+
+  /** копируем файл */
+  async ['copy - файл']() {
+    const files = []
+
+    await mkdir('test/example/fs/copy')
+    await copy('test/example/fs/walk/f1.txt', 'test/example/fs/copy/f1.txt')
+    await walk('test/example/fs/copy', path => files.push(path))
+    await rmdir('test/example/fs/copy', { recursive: true })
+
+    assert.deepEqual(files, ['f1.txt'])
+  }
+
+  /** копируем файл, если каталог назначения не создан */
+  async ['copy - файл (без каталога)']() {
+    const files = []
+
+    await copy('test/example/fs/walk/f1.txt', 'test/example/fs/copy/f1.txt')
+    await walk('test/example/fs/copy', path => files.push(path))
+    await rmdir('test/example/fs/copy', { recursive: true })
+
+    assert.deepEqual(files, ['f1.txt'])
   }
 
 }

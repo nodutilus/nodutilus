7a92eb22da627c2bb8d5bf2b4c843ed8c494d4e8
IgorNovozhilov <maildrakon@gmail.com>
2019-10-03 23:15:14 +0300

    init package.json build

--- bin/build-multi-package.js
+++ bin/build-multi-package.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { readJSON, copy, constants: { COPY_RMNONEXISTENT } } = require('../src/@ndk/fs')
+const { readJSON, writeJSON, copy, constants: { COPY_RMNONEXISTENT } } = require('../src/@ndk/fs')
 const { readdir } = require('fs').promises
 
 
@@ -10,17 +10,63 @@ const { readdir } = require('fs').promises
  */
 async function buildMultiPackage(optionsFile) {
   const options = await readJSON(optionsFile)
-  const packageNames = await readdir(options.packages)
-  const packages = await Promise.all(packageNames.map(async name => {
+  const packageDirs = await readdir(options.packages)
+  const packagesSrc = await Promise.all(packageDirs.map(async name => {
     const packageJSON = await readJSON(`${options.packages}/${name}/package.json`)
 
-    return packageJSON
+    return [name, packageJSON]
   }))
-  const versions = await readJSON(options.versions, {})
+  const versions = await readJSON(options.versions)
+  const packageAliases = {}
+  const packageNames = []
+  const packages = {}
 
   await copy(options.packages, options.buildFolder, COPY_RMNONEXISTENT)
+  for (const [name, pckg] of packagesSrc) {
+    const packageJSON = Object.assign({}, options.template, pckg)
+    const verInfo = versions[packageJSON.name]
 
-  console.log('buildMultiPackage:', options, packages, versions)
+    if (verInfo.version && verInfo.publish) {
+      packageJSON.version = verInfo.version
+    }
+    packageAliases[packageJSON.name] = name
+    packages[packageJSON.name] = packageJSON
+    packageNames.push(packageJSON.name)
+  }
+
+  for (const name of packageNames) {
+    const packageJSON = packages[name]
+    const crossDependencies = options.crossDependencies[name]
+    const dependencies = {}
+
+    if (crossDependencies) {
+      crossDependencies.forEach(crossName => {
+        const crossVersion = packages[crossName].version
+
+        if (!crossVersion) {
+          throw new Error(`"${crossName}" - Not found`)
+        }
+        dependencies[crossName] = crossVersion
+      })
+    }
+
+    if (Object.keys(dependencies).length > 0) {
+      packageJSON.dependencies = dependencies
+    }
+  }
+
+  for (const packageJSON of Object.values(packages)) {
+    const alias = packageAliases[packageJSON.name]
+
+    for (const prop in packageJSON) {
+      if (packageJSON[prop] === null) {
+        delete packageJSON[prop]
+      }
+    }
+    await writeJSON(`${options.buildFolder}/${alias}/package.json`, packageJSON)
+  }
+
+  console.log('buildMultiPackage:', options, packagesSrc, versions)
 }
 
 

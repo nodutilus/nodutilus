350441a8aa8d4d8eaf956759c7a20124423859c2
Igor Novozhilov <maildrakon@gmail.com>
2021-03-12 17:13:45 +0300

    test

--- test/pre-test.js
+++ test/pre-test.js
@@ -1,17 +1,13 @@
 import { mkdirSync, symlinkSync, existsSync } from 'fs'
-import { resolve } from 'path'
 
-console.log(process.cwd())
-console.log(resolve('test/node_modules'))
-console.log(resolve('@nodutilus'))
-console.log(resolve('test/node_modules/@nodutilus'))
-console.log(resolve('test/@nodutilus-test'))
-console.log(resolve('test/node_modules/@nodutilus-test'))
-
-mkdirSync(resolve('test/node_modules'), { recursive: true })
-if (!existsSync(resolve('test/node_modules/@nodutilus'))) {
-  symlinkSync('../../@nodutilus', resolve('test/node_modules/@nodutilus'), 'dir')
+mkdirSync('@nodutilus/node_modules', { recursive: true })
+if (!existsSync('@nodutilus/node_modules/@nodutilus')) {
+  symlinkSync('../../@nodutilus', '@nodutilus/node_modules/@nodutilus', 'dir')
 }
-if (!existsSync(resolve('test/node_modules/@nodutilus-test'))) {
-  symlinkSync('../@nodutilus-test', resolve('test/node_modules/@nodutilus-test'), 'dir')
+mkdirSync('test/node_modules', { recursive: true })
+if (!existsSync('test/node_modules/@nodutilus')) {
+  symlinkSync('../../@nodutilus', 'test/node_modules/@nodutilus', 'dir')
+}
+if (!existsSync('test/node_modules/@nodutilus-test')) {
+  symlinkSync('../@nodutilus-test', 'test/node_modules/@nodutilus-test', 'dir')
 }
